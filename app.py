3e13b85933560fd85be25c74c57253673b217b27
DarioFesta <47891826+DarioFesta@users.noreply.github.com>
2021-03-22 16:12:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import plotly.graph_objects as go
 
 
 #st.set_page_config(layout="wide")
-st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
+
 st.title('Data Plots / Stats')
 st.subheader('Create plots and show main stats for each parameter')
 
